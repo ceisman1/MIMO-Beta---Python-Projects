d4348fde6b0c85447ec1ff13b03eb7df3cf5a322
Christine Eisman <89891365+ceisman1@users.noreply.github.com>
2023-08-07 00:27:31 -0400

    Intelligent Car Project Commit

---
+++ IntelligentCar.py
@@ -0,0 +1,58 @@
+class Car:
+    def __init__(self, fuel_consumption, possible_destinations):
+        self.fuel_consumption = fuel_consumption
+        self.possible_destinations = possible_destinations
+        self.total_distance = 0
+        self.distance_left = 0
+
+    def ride(self, distance):
+        try:
+            self.check_distance(distance)
+        except Exception as e:
+            print(e)
+        else:
+            self.total_distance += distance
+            self.distance_left -= distance
+            print(f"The ride was successfull! Distance traveled: {distance}")
+    
+    def check_distance(self, distance):
+        if self.distance_left < distance:
+            raise ValueError("Not enough fuel, recharge the car!")
+
+    def calculate_average_speed(self, distance, duration):
+        try:
+            average_speed = distance / duration
+            return average_speed
+        except ZeroDivisionError:
+            return distance
+
+    def ride_to_destination(self, destination):
+        if destination in self.possible_destinations:
+            distance = self.possible_destinations[destination]
+            self.ride(distance)
+            print(f"Reached {destination}!")
+        else:
+            print(f"Destination not in possible destinations!")
+
+    def recharge(self, fuel):
+        self.distance_left += fuel * self.fuel_consumption
+        print(f"Car charged successfully! Distance left to ride: {self.distance_left}.")
+
+possible_destinations = {
+    "Eriador": 100,
+    "Gondor": 50
+}
+
+ford_mustang = Car(14, possible_destinations)
+ford_mustang.ride(10)
+ford_mustang.recharge(10)
+ford_mustang.ride(50)
+
+average_speed = ford_mustang.calculate_average_speed(150, 2)
+print(f"Average speed: {average_speed}")
+
+average_speed = ford_mustang.calculate_average_speed(150, 0)
+print(f"Average speed: {average_speed}")
+
+ford_mustang.ride_to_destination("Gondor")
+ford_mustang.ride_to_destination("Wonderland")
